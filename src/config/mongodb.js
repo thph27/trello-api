c1e6f1ce7a8c822cf6a5be18869a8c2cf9f6484d
thph27 <118583277+thph27@users.noreply.github.com>
2024-06-12 14:09:52 +0700

    connect_mongodb_env_variables_p2

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,16 +1,11 @@
-
-
-// usename: thanggpham27
-// Mk: JOwJQpHk2Y670G15
-
-const MONGODB_URI = 'mongodb+srv://thanggpham27:JOwJQpHk2Y670G15@cluster0-thangpham27.kswpdno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-ThangPham27'
-const DATABASE_NAME = 'trello-thph27'
-
 import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from '~/config/environment'
+
+
 // Khoi tao doi tuong trelloDatabaseInstance ban dau la null vi chua connect
 let trelloDatabaseInstance = null
 // Khoi tao 1 doi tuong MongoClientInstance de connect toi MongoDB
-const mongoClientInstance = new MongoClient(MONGODB_URI, {
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -21,7 +16,7 @@ export const CONNECT_DB = async () => {
   // goi ket noi toi MongoDB Atlas voi URI da khai bao
   await mongoClientInstance.connect()
   // Ket noi thanh cong thi lay Database ra theo ten va gan nguoc lai vao bien trelloDatabaseInstance
-  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
 }
 export const CLOSE_DB = async () => {
   await mongoClientInstance.close()
--- src/server.js
+++ src/server.js
@@ -2,28 +2,23 @@
 
 import express from 'express'
 import exitHook from 'async-exit-hook'
-import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
 
 const START_SERVER = () => {
   const app = express()
 
-  const hostname = 'localhost'
-  const port = 8017
-
   app.get('/', async (req, res) => {
-    console.log(await GET_DB().listCollections().toArray())
-    process.exit(0)
     res.end('<h1>Hello World!</h1><hr>')
   })
 
-  app.listen(port, hostname, () => {
-    console.log(`3. Hello Thph27, I am running successfull at Host: ${hostname} and Port: ${port}`)
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`3. Hello ${env.AUTHOR}, I am running successfull at Host: ${env.APP_HOST} and Port: ${env.APP_PORT}`)
   })
 
   exitHook(() => {
-    console.log('4. Disconnecting to Mongo Cloud Atlas.....')
+    console.log('4. Server is shutting down.')
     CLOSE_DB ()
-    console.log('5. Disconnected to Mongo Cloud Atlas')
   })
 }
 // Chi khi connect Database thanh cong thi moi start Server Back-end
