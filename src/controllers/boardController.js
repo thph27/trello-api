99de7584778a0426e02c0d748c1f091ea436087a
thph27 <118583277+thph27@users.noreply.github.com>
2024-06-15 10:02:56 +0700

    Api get details of Board

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -11,8 +11,16 @@ const createNew = async(req, res, next) => {
     res.status(StatusCodes.CREATED).json(createdBoard)
   } catch (error) { next(error) }
 }
+const getDetails = async(req, res, next) => {
+  try {
+    const boardId = req.params.id
+    const board = await boardService.getDetails(boardId)
+    res.status(StatusCodes.OK).json(board)
+  } catch (error) { next(error) }
+}
 
 
 export const boardController = {
-  createNew
+  createNew,
+  getDetails
 }
