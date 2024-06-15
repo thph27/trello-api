bd4aaa4a66638b0f93a19951ea0888b046c88149
thph27 <118583277+thph27@users.noreply.github.com>
2024-06-15 09:34:41 +0700

    Checking Data

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,4 +1,5 @@
 import Joi from 'joi'
+import { ObjectId } from 'mongodb'
 import { GET_DB } from '~/config/mongodb'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 // define Collection (Name & Schema)
@@ -19,9 +20,14 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
 
 })
 
+const validateBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
+}
+
 const createNew = async(data) => {
   try {
-    const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data)
+    const validData = await validateBeforeCreate(data)
+    const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(validData)
     return createdBoard
   } catch (error) {
     throw new Error(error)
@@ -30,9 +36,7 @@ const createNew = async(data) => {
 
 const findOneById = async (id) => {
   try {
-    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
-      _id: id
-    })
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({ _id: new ObjectId(id) })
     return result
   } catch (error) {
     throw new Error(error)
