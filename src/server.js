faffc836d4ad6e9080b54e2ac1b5d7e7037cfa70
thph27 <118583277+thph27@users.noreply.github.com>
2024-07-09 14:26:16 +0700

    Deploy Production (Render)

--- src/server.js
+++ src/server.js
@@ -18,10 +18,18 @@ const START_SERVER = () => {
   app.use('/v1', APIs_V1)
   // Middleware xu li loi tap trung
   app.use(errorHandlingMiddleware)
-
-  app.listen(env.APP_PORT, env.APP_HOST, () => {
-    console.log(`3. Hello ${env.AUTHOR}, I am running successfull at Host: ${env.APP_HOST} and Port: ${env.APP_PORT}`)
-  })
+  
+  // Mtrg production (cu the la dang support Render.com)
+  if (env.BUILD_MODE === 'production') {
+    app.listen(process.env.PORT, () => {
+      console.log(`3. Production: Hello ${env.AUTHOR}, Back-end Server is running successfull at Port: ${process.env.PORT}`)
+    })
+  } else { 
+    // Mtrg local dev
+    app.listen(env.LOCAL_DEV_APP_PORT, env.LOCAL_DEV_APP_HOST, () => {
+      console.log(`3. Local DEV: Hello ${env.AUTHOR}, I am running successfull at Host: ${env.LOCAL_DEV_APP_HOST} and Port: ${env.LOCAL_DEV_APP_PORT}`)
+    })
+  }
 
   exitHook(() => {
     console.log('4. Server is shutting down.')
