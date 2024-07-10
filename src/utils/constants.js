8e72344c3e76e4b4e76c3c945db31a951e0a3c8a
thph27 <118583277+thph27@users.noreply.github.com>
2024-07-10 15:24:00 +0700

    Update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
   // Khong can local host vi file config/cors de luon luon cho phep mtrg dev
-  'https://trello-web-five-iota.vercel.app'
+  'https://trello-web-thph27.vercel.app'
 ]
 export const BOARD_TYPES = {
   PUBLIC: 'public',
