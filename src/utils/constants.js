abc11f0360d91d44646015ba2d451ff0e758ea5b
thph27 <118583277+thph27@users.noreply.github.com>
2024-07-10 15:04:56 +0700

    Update Whitelist Domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,7 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
   // Khong can local host vi file config/cors de luon luon cho phep mtrg dev
+  'https://trello-web-five-iota.vercel.app/'
 ]
 export const BOARD_TYPES = {
   PUBLIC: 'public',
